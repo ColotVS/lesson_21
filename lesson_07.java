e4949415bf6119ed083c5b0d275709cda51e44d4
ColotVS <kolotilov98@mail.ru>
2024-04-03 10:35:47 +0700

    Наследование. Практика 5 Урок 21, задача 7

---
+++ lesson_07.java
@@ -0,0 +1,43 @@
+public class lesson_07 {
+    //Исправь девять классов: Worker (сотрудник), Clerk (клерк), IT (ИТ-специалист),
+    //Programmer (программист), ProjectManager (менеджер проекта), CTO (технический директор),
+    //HR (рекрутер), OfficeManager (офис-менеджер), Cleaner (уборщик).
+    //Унаследуй программиста, менеджера проекта и технического директора от ИТ-специалиста.
+    //Унаследуй рекрутера, уборщика и офис-менеджера от клерка.
+    //Унаследуй клерка и ИТ-специалиста от сотрудника.
+}
+class Worker {
+
+}
+
+class Clerk extends Worker{
+
+}
+
+class IT extends Worker{
+
+}
+
+class Programmer extends IT{
+
+}
+
+class ProjectManager extends IT{
+
+}
+
+class CTO extends IT{
+
+}
+
+class OfficeManager extends Clerk{
+
+}
+
+class HR extends Clerk{
+
+}
+
+class Cleaner extends Clerk{
+
+}
