900a6d452fed7c0c5d06c8a8e9ea2d82f3e38382
ColotVS <kolotilov98@mail.ru>
2024-04-03 16:19:09 +0700

    Инкапсуляция, Практика 5 Урок 21, задача 13

---
+++ lesson_13.java
@@ -0,0 +1,43 @@
+public class lesson_13 {
+    //Посмотри внимательно на методы и добавь недостающие поля.
+}
+class Cat6 {
+    private String name;
+    private int age;
+    private int weight;
+    private int speed;
+
+    public Cat6(String name, int age, int weight, int speed) {
+
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
