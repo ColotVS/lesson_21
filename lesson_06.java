f9e57f2e7501e3417990bafbe617230e7ce66d70
ColotVS <kolotilov98@mail.ru>
2024-04-03 10:34:37 +0700

    Наследование. Практика 4 Урок 21, задача 6

---
+++ lesson_06.java
@@ -0,0 +1,17 @@
+public class lesson_06 {
+    //Напиши четыре класса: Employee (сотрудник), Manager (управляющий), Chief (директор) и Secretary (секретарь).
+    //Унаследуй управляющего, директора и секретаря от сотрудника.
+}
+class Employee{
+
+}
+class Manager extends Employee{
+
+}
+class Chief extends Employee{
+
+}
+class Secretary extends Employee{
+
+}
+
