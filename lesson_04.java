1ffa52f35c55d4d50ea55fbe1673b040b006080d
ColotVS <kolotilov98@mail.ru>
2024-04-03 10:28:32 +0700

    Наследование. Практика 2 Урок 21, задача 4

---
+++ lesson_04.java
@@ -0,0 +1,14 @@
+public class lesson_04 {
+    //Отредактируй три класса: Pet (домашнее животное), Cat (кот) и Dog (собака).
+    //Унаследуй кота и собаку от животного.
+}
+
+class Pet{
+
+}
+class Cat extends Pet{
+
+}
+class Dog extends Pet{
+
+}
