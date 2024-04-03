32bbb423336d3943d11ed2c7e3d1f29fd83d5359
ColotVS <kolotilov98@mail.ru>
2024-04-03 10:27:26 +0700

    Наследование. Практика 1 Урок 21, задача 3

---
+++ lesson_03.java
@@ -0,0 +1,10 @@
+public class lesson_03 {
+    //Отредактируй два класса — Horse (лошадь) и Pegasus (пегас).
+    //Унаследуй пегаса от лошади.
+}
+class Horse{
+
+}
+class Pegasus extends Horse{
+
+}
