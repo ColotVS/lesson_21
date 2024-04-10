54a5844820777e7f4a1f7dbea3d21513b45f69a1
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-10 17:17:44 +0700

    Бонусное задание Практика 3 Урок 21, задача 16

---
+++ lesson_16.java
@@ -0,0 +1,20 @@
+public class lesson_16 {
+    //Измени четыре класса: Fish (Рыба), Animal (Животное), Ape (Обезьяна), Human (Человек).
+    //Унаследовать животное от рыбы, обезьяну — от животного и человека от обезьяны.
+}
+class Fish {
+
+}
+
+class Animal2 extends Fish{
+
+}
+
+class Ape extends Animal2{
+
+}
+
+class Human extends Ape{
+
+}
+
