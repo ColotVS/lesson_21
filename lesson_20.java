6fc7fb1c9fb6de854e6e4dfbf7ce8d4a65511ead
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-10 17:22:42 +0700

    Бонусное задание Практика 7 Урок 21, задача 20

---
+++ lesson_20.java
@@ -0,0 +1,23 @@
+public class lesson_20 {
+    //Расставь правильно «цепочку наследования» в классах:
+    //Carnivore (плотоядное животное), Cow (корова), Dog (собака), Pig (свинья), Animal (животное).
+}
+class Carnivore extends Animal3{
+
+}
+
+class Cow2 extends Animal3{
+
+}
+
+class Dog8 extends Carnivore{
+
+}
+
+class Pig2 extends Animal3{
+
+}
+
+class Animal3 {
+
+}
