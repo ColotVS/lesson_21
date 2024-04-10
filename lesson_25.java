121caa90412f5938632f192688b40dc611d73931
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-10 17:28:01 +0700

    Бонусное задание Практика 12 Урок 21, задача 25

---
+++ lesson_25.java
@@ -0,0 +1,24 @@
+public class lesson_25 {
+    //Добавь общий базовый класс ChessPieces к классам фигур (фигуры из шахмат).
+}
+class ChessPieces{
+
+}
+
+class King extends ChessPieces{
+}
+
+class Queen extends ChessPieces{
+}
+
+class Rook extends ChessPieces{
+}
+
+class Knight extends ChessPieces{
+}
+
+class Bishop extends ChessPieces{
+}
+
+class Pawn extends ChessPieces{
+}
