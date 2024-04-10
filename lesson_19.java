a1ad61ac60af0ba8ab857bb90c551be113bfa2e5
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-10 17:21:28 +0700

    Бонусное задание Практика 6 Урок 21, задача 19

---
+++ lesson_19.java
@@ -0,0 +1,14 @@
+public class lesson_19 {
+    //Расставь правильно «цепочку наследования» в классах: Pet (домашнее животное), Cat (кот), Dog (собака).
+}
+class Pet2 {
+
+}
+
+class Cat7 extends Pet2{
+
+}
+
+class Dog7 extends Pet2{
+
+}
