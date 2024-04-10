ac92181553290a490eacf9a5ae77e83e21f1cb95
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-10 17:27:03 +0700

    Бонусное задание Практика 11 Урок 21, задача 24

---
+++ lesson_24.java
@@ -0,0 +1,24 @@
+public class lesson_24 {
+    //Исправь наследование в классах
+    //Cat, Dog, Pet, House, Airplane, чтобы программа компилировалась и работала.
+
+}
+class Pet12 {
+
+}
+
+class Cat12 extends Pet12 {
+
+}
+
+class Dog12 extends Pet12 {
+
+}
+
+class House12{
+
+}
+
+class Airplane {
+
+}
