632a344b3a1e92c6c76959650b4e9fe7f1159ef8
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-10 17:18:46 +0700

    Бонусное задание Практика 4 Урок 21, задача 17

---
+++ lesson_17.java
@@ -0,0 +1,15 @@
+public class lesson_17 {
+    //Измени три класса: Fish (рыба), Lizard (ящерица), Dinosaur (динозавр).
+    //Унаследуй ящерицу от рыбы и динозавра от ящерицы.
+}
+class Fish2 {
+
+}
+
+class Lizard extends Fish2{
+
+}
+
+class Dinosaur extends Lizard{
+
+}
