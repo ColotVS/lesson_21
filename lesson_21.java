5ab63ccc943691c1a37bf7dd47ed8974af55960a
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-10 17:24:02 +0700

    Бонусное задание Практика 8 Урок 21, задача 21

---
+++ lesson_21.java
@@ -0,0 +1,19 @@
+public class lesson_21 {
+    //Расставь правильно «цепочку наследования» в классах:
+    //Pet (домашнее животное), Cat (кот), Dog (собака), Car (машина).
+}
+class Pet3 {
+
+}
+
+class Cat9 extends Pet3{
+
+}
+
+class Car {
+
+}
+
+class Dog9 extends Pet3{
+
+}
