354058b3ea719248fe4488e8d61c3ef2ba666d25
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-10 17:25:59 +0700

    Бонусное задание Практика 10 Урок 21, задача 23

---
+++ lesson_23.java
@@ -0,0 +1,27 @@
+public class lesson_23 {
+    //Расставь правильно «цепочку наследования» в классах:
+    //House (дом), Cat (кот), Dog (собака), Car (машина), Animal (животное), Asset (имущество).
+}
+class House2 extends Asset{
+
+}
+
+class Cat11 extends Animal11{
+
+}
+
+class Car11 extends Asset {
+
+}
+
+class Dog11 extends Animal{
+
+}
+
+class Animal11 {
+
+}
+
+class Asset {
+
+}
