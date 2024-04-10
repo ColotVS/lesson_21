41c8d715c936609550b18451cea71dc5c8699442
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-10 17:19:30 +0700

    Бонусное задание Практика 5 Урок 21, задача 18

---
+++ lesson_18.java
@@ -0,0 +1,19 @@
+public class lesson_18 {
+    //Измени четыре класса: Schoolboy (школьник), Student (студент), Worker (Сотрудник), Retiree (Пенсионер).
+    //Унаследовать студента от школьника, сотрудника от студента, пенсионера от сотрудника.
+}
+class Schoolboy {
+
+}
+
+class Student extends Schoolboy{
+
+}
+
+class Worker2 extends Student{
+
+}
+
+class Retiree extends Worker2{
+
+}
