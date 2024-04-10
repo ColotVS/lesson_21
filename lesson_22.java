0d38ff772f7efdf1e1597bbabfe82d501825a13c
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-10 17:24:54 +0700

    Бонусное задание Практика 9 Урок 21, задача 22

---
+++ lesson_22.java
@@ -0,0 +1,19 @@
+public class lesson_22 {
+    //Расставь правильно «цепочку наследования» в классах:
+    //House (дом), Cat (кот), Dog (собака), Car (машина).
+}
+class House {
+
+}
+
+class Cat10{
+
+}
+
+class Car10 {
+
+}
+
+class Dog10{
+
+}
